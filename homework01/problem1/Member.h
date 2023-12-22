a152d2bc530633f4d02aaed29d4a53d4e13cad59
Allen-Sunboy <yangnan_sunboy@163.com>
2023-12-22 17:52:26 +0800

    detail

--- homework01/problem1/Member.h
+++ homework01/problem1/Member.h
@@ -2,15 +2,14 @@
 #include <string>
 
 class Member {
-    private:
-        std::string name;
-        int age;
+private:
+    std::string name;
+    int age;
 
-    public:
-        Member(std::string Name, int Age) : name(Name), age(Age) {}
-        Member() : name("?"), age(0) {}
-        friend std::ostream &operator<<(std::ostream &out,
-                                        const Member &member);
-        std::string getname();
-        int getage();
+public:
+    Member(std::string Name, int Age) : name(Name), age(Age) {}
+    Member() : name("?"), age(0) {}
+    friend std::ostream &operator<<(std::ostream &out, const Member &member);
+    std::string getname();
+    int getage();
 };
